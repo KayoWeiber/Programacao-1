b2cdd8c3d4f8b2f6cbf2ca1bc9cd71f9758ed522
Kayo Weiber <caioveiber598@gmail.com>
2025-05-08 19:34:51 -0300

    Aula-08-05

---
+++ aula-08-05-25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_08_05_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

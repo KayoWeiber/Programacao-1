13c6997de45828774cc7473f0e83af132c3bd040
Kayo Weiber <caioveiber598@gmail.com>
2025-05-29 20:34:01 -0300

    feat(aula-29-05) - Studies on the concept of "Inheritance" and "Polymorphism"

---
+++ Aula-29-05/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_29_05
+{
+    internal class Program
+    {
+        public class veiculos
+        {
+            public string modelo { get; set; }
+            public int ano { get; set; }
+
+            public veiculos(string modelo, int ano)
+            {
+                this.modelo = modelo;
+                this.ano = ano;
+            }
+            public void MostrarVeiculo()
+            {
+                Console.WriteLine($"Modelo: {modelo}, Ano de fabricação: {ano}");
+            }
+        }
+
+        public class carro : veiculos 
+
+        { 
+            public int numeroPortas { get; set; }
+
+            public carro(string modelo, int ano, int numeroPortas) : base(modelo,ano)
+            {
+                this.numeroPortas = numeroPortas;
+            }
+
+            public void exibirPortas()
+            {
+                MostrarVeiculo();
+                Console.WriteLine($"Numeros de portas: {numeroPortas}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            carro veiculo1 = new carro("Fusca",1960,2);
+            veiculo1.modelo = "Fusca";
+           //veiculo1.ano = 1969;
+            //veiculo1.numeroPortas = 2;
+            //veiculo1.MostrarVeiculo();
+            veiculo1.exibirPortas();
+            veiculos veiculo2 = new veiculos("Uno",1990);
+           
+            veiculo2.MostrarVeiculo();
+
+        }
+    }
+}
---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        public class veiculo
+        {
+            public virtual void mover()
+            {
+                Console.WriteLine("O veiculo está se movendo");
+            }
+        }
+        public class Carro : veiculo 
+        {
+            public override void mover() 
+            {
+                Console.WriteLine("O carro está se movendo");
+            }
+        }
+        public class Moto : veiculo 
+        {
+            public override void mover() {
+                Console.WriteLine("Moto está se movendo.");
+                base.mover();
+            
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            veiculo veiculos = new veiculo();
+            Carro carro = new Carro();
+            Moto moto = new Moto();
+            carro.mover();
+            veiculos.mover();
+            moto.mover();
+            veiculo[] veiculosVetor = new veiculo[3];
+            veiculosVetor[0] = new veiculo();
+            veiculosVetor [1] = new Carro();
+            veiculosVetor [2] = new Moto();
+
+        }
+    }
+}

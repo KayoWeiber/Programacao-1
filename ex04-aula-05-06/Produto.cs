eb4903e001e306e4f4445f268d7a1966a885c0ac
Kayo Weiber <caioveiber598@gmail.com>
2025-06-10 21:44:22 -0300

    feat(ex04) -  exercise conclusion

---
+++ ex04-aula-05-06/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex04_aula_05_06
+{
+    internal class Produto
+    {
+        public string nome { get; set; }
+        public double preco { get; set; }
+
+        public Produto(string nome, double preco ) { this.nome = nome;this.preco = preco; }
+        public override string ToString()
+        {
+            return $"Produto: {nome}, Preço: R${preco}";
+        }
+    }
+}
--- ex04-aula-05-06/Program.cs
+++ ex04-aula-05-06/Program.cs
@@ -10,6 +10,8 @@ namespace ex04_aula_05_06
     {
         static void Main(string[] args)
         {
+            Produto produtoTeste = new Produto("Maconha",10);
+            Console.WriteLine(produtoTeste);
         }
     }
 }

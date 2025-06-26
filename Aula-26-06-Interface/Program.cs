a30424f64dee762fc9d50bc01b3c4ffcd058f99e
Kayo Weiber <caioveiber598@gmail.com>
2025-06-26 20:34:55 -0300

    feat(aula-26-06) - Abstract and Interface

---
+++ Aula-26-06-Interface/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_26_06_Interface
+{
+    internal class Program
+    {
+        interface IVoador
+        {
+            void Voar();
+            void comer();
+        }
+        interface INadador
+        {
+            int distancia { get; set; }
+            void Nadar();
+            void comer();
+        }
+        public class passaro : IVoador
+        {
+            public void comer()
+            {
+                Console.WriteLine("comendo");
+            }
+
+            public void Voar()
+            {
+                Console.WriteLine("Voando");
+            }
+        }
+        public class pinguin : INadador
+        {
+            public int distancia { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public void comer()
+            {
+                Console.WriteLine("comendo");
+            }
+
+            public void Nadar()
+            {
+                Console.WriteLine($"nadando");
+            }
+        }
+        public class pato : INadador, IVoador
+        {
+            public int distancia { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public void comer()
+            {
+                Console.WriteLine("comendo");
+            }
+
+            public void Nadar()
+            {
+                Console.WriteLine($"nadando");
+            }
+
+            public void Voar()
+            {
+                Console.WriteLine("Voando");
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Aula-26-06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_26_06
+{
+    internal class Program
+    {
+        public abstract class funcionario
+        {
+            public string matricula { get; set; }
+            public string nome { get; set; }
+
+            public void mostrar_funcionario()
+            {
+                Console.WriteLine($"Matricula {0} - Nome {1}",this.matricula,this.nome);
+            }
+            public abstract double comissao();
+
+        }
+        public class vendedor : funcionario
+        {
+            public override double comissao()
+            {
+                return Double.Parse("0,1");
+            }
+        }
+        public class atendente : funcionario
+        {
+            public override double comissao()
+            {
+                return Double.Parse("0,05");
+            }
+        }
+        public class Gerente : funcionario
+        {
+            public override double comissao()
+            {
+                return Double.Parse("0,2");
+            }
+        }
+        static void Main(string[] args)
+        {
+            vendedor joao=new vendedor();
+            Gerente Kayo=new Gerente();
+            atendente Murilo =new atendente();
+
+            Console.WriteLine("Comissão do Kayo {0}%",Kayo.comissao()*100);
+            Console.WriteLine("Comissão do Joao {0}%", joao.comissao()*100);
+            Console.WriteLine("Comissão do Murilo {0}%", Murilo.comissao() * 100);
+        }
+    }
+}
